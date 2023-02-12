1f27c0a82e4cd097dc3b1c3689ef353b464711f2
Alan Solidum <hi@alansolidum.com>
2023-02-12 08:10:45 -0800

    feat: Calculate surfer's average ages

--- analysis.py
+++ analysis.py
@@ -5,3 +5,6 @@ surfers = pd.read_csv("surfers.csv")
 category = surfers['Category'].value_counts(normalize=True) * 100
 home = surfers['Home'].value_counts(normalize=True) * 100
 stance = surfers['Stance'].value_counts(normalize=True) * 100
+avg_age = surfers[['Age']].mean()
+avg_age_male = surfers.loc[surfers['Category'] == 'M']['Age'].mean()
+avg_age_female = surfers.loc[surfers['Category'] == 'F']['Age'].mean()
