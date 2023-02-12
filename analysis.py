1b4c7bf7d85f15209fb70c8542a41b3d5b701f4c
Alan Solidum <hi@alansolidum.com>
2023-02-11 16:05:17 -0800

    refactor: Rename variables for easier readability

--- analysis.py
+++ analysis.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-surfers_df = pd.read_csv("surfers.csv")
+surfers = pd.read_csv("surfers.csv")
 
-category_df = surfers_df['Category'].value_counts(normalize=True) * 100
-home_df = surfers_df['Home'].value_counts(normalize=True) * 100
-stance_df = surfers_df['Stance'].value_counts(normalize=True) * 100
+category = surfers['Category'].value_counts(normalize=True) * 100
+home = surfers['Home'].value_counts(normalize=True) * 100
+stance = surfers['Stance'].value_counts(normalize=True) * 100
