5d7a39237e7886c2a2ae908ea982d7f493c67529
Alan Solidum <hi@alansolidum.com>
2023-02-13 17:21:56 -0800

    refactor: Put average age calcuations into function

--- analysis.py
+++ analysis.py
@@ -1,10 +1,19 @@
 import pandas as pd
 
+def get_average_ages(surfers):
+    avg_ages = dict()
+
+    avg_ages['all'] = surfers[['Age']].mean()
+    avg_ages['male'] = surfers.loc[surfers['Category'] == 'M']['Age'].mean()
+    avg_ages['female'] = surfers.loc[surfers['Category'] == 'F']['Age'].mean()
+
+    return avg_ages
+
 surfers = pd.read_csv("surfers.csv")
 
 category = surfers['Category'].value_counts(normalize=True) * 100
 home = surfers['Home'].value_counts(normalize=True) * 100
 stance = surfers['Stance'].value_counts(normalize=True) * 100
-avg_age = surfers[['Age']].mean()
-avg_age_male = surfers.loc[surfers['Category'] == 'M']['Age'].mean()
-avg_age_female = surfers.loc[surfers['Category'] == 'F']['Age'].mean()
+
+avg_ages = get_average_ages(surfers)
+
