eef4434a2fe30409d5154c331355c34409d458e6
Alan Solidum <hi@alansolidum.com>
2023-02-18 00:18:06 -0800

    refactor: Create functions to return ages for male and female surfers

--- analysis.py
+++ analysis.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
+def get_male_ages(surfers):
+    return surfers.loc[surfers['Category'] == 'M']['Age']
+
+def get_female_ages(surfers):
+    return surfers.loc[surfers['Category'] == 'F']['Age']
+
 def get_average_ages(surfers):
     avg_ages = dict()
 
     avg_ages['all'] = round(surfers[['Age']].mean()[0], 1)
-    avg_ages['male'] = round(surfers.loc[surfers['Category'] == 'M']['Age'].mean(), 1)
-    avg_ages['female'] = round(surfers.loc[surfers['Category'] == 'F']['Age'].mean(), 1)
+    avg_ages['male'] = round(get_male_ages(surfers).mean(), 1)
+    avg_ages['female'] = round(get_female_ages(surfers).mean(), 1)
 
     return avg_ages
 
@@ -19,9 +25,9 @@ def plot_age_histogram(surfers):
 
     avg_ages = get_average_ages(surfers)
 
-    plt.hist(surfers.loc[surfers['Category'] == 'M']['Age'], bins=age_bins, alpha=alpha, color=male_color, label='Men')
+    plt.hist(get_male_ages(surfers), bins=age_bins, alpha=alpha, color=male_color, label='Men')
     plt.axvline(avg_ages['male'], color=male_color, linestyle='dashed', linewidth=mean_line_width)
-    plt.hist(surfers.loc[surfers['Category'] == 'F']['Age'], bins=age_bins, alpha=alpha, color=female_color, label='Women')
+    plt.hist(get_female_ages(surfers), bins=age_bins, alpha=alpha, color=female_color, label='Women')
     plt.axvline(avg_ages['female'], color=female_color, linestyle='dashed', linewidth=mean_line_width)
     plt.legend(loc='upper right')
     plt.show()
