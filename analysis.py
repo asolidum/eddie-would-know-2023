91cfb26d13daecb95c8ce73faf2c5861a11a63e1
Alan Solidum <hi@alansolidum.com>
2023-03-08 08:50:18 -0800

    refactor: Pull heat total functionality into get_heat_total function

--- analysis.py
+++ analysis.py
@@ -110,11 +110,16 @@ def get_surfer_heat_total(round_num, surfer_heat_data):
     
     return surfer_heat_data[range(2, end_range)].sum()
 
-def analyze_heat(round_num, heat_num, round_data):
-    heat_data = get_heat(heat_num, round_data)
+def get_heat_total(round_num, heat_data):
     total = 0
     for index, row in heat_data.iterrows():
         total += get_surfer_heat_total(round_num, row)
+
+    return total
+
+def analyze_heat(round_num, heat_num, round_data):
+    heat_data = get_heat(heat_num, round_data)
+    total = get_heat_total(round_num, heat_data)
 
     total_heat_intervals = 88 if round_num == 2 else 80
     avg_score = round(total/total_heat_intervals, 2)
